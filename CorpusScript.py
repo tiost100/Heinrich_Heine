78225d68c21eab3b54a4a09e57bd6e4459cf192e
Tim Ostrolucký <115794574+tiost100@users.noreply.github.com>
2023-12-20 13:21:33 +0100

    Corpus (1st version) & updated script

--- CorpusScript.py
+++ CorpusScript.py
@@ -1,26 +1,52 @@
 import os
 import re
+import csv
 
 # Get the full path of the directory where the current file is located
 dir_path = os.path.dirname(os.path.abspath(__file__))
 dir_path = dir_path.replace('\\','/')
 
 
-def createScript(filepath, author, year, genre, source):
-    text = ""
+def createCorpus(filepath):
+    """Create a corpus from the .txt-files
 
-    with open(filepath, "r", encoding="latin-1") as infile:
-        for line in infile:
-            if line.strip():
-                text += line.strip("\n") + " "
+    args: filepath (string, full path of the location of the files)
 
-        text = re.sub(";", ",", text)
+    return: none
 
-    sentences = text.split(".")
+    note: the file path depends on the storage location of the file on the 
+    computer, and can vary from computer to computer
+    """
 
-    with open(f"{dir_path}/Lyrisch.csv", "w", encoding="latin-1") as outfile:
-        for i in range(len(sentences)):
-            outfile.write(f"{i};{sentences[i].strip()};{author};{year};{genre};{source}\n") 
+    # open the textinfo.csv-file with all the information about every text
+    with open(filepath + "/textinfo.csv") as csvfile:
+        csv_reader_object = csv.reader(csvfile, delimiter=";")
+        row_nr = 0
 
-data_text = f"{dir_path}/Lyrisch.txt"
-createScript(data_text, "Heinrich Heine", "n.n.", "lyrical", "n.n.")
+        # go through each row in the .csv-file
+        for row in csv_reader_object:
+            # skip the first line as there are only the column names
+            if row_nr != 0:
+                text_nr = row[0]
+                text = ""
+                
+                # open current .txt-file and store all the text data into variable text
+                with open(filepath+ "/Textdaten/" + text_nr + ".txt", "r", encoding="latin-1") as infile:
+                    for line in infile:
+                        if line.strip():
+                            text += line.strip("\n") + " "
+
+                    text = re.sub(";", ",", text)
+
+                # split the text into sentences
+                sentences = text.split(".")
+                sentences = sentences[:-1]
+                sentences_nr = len(sentences)
+
+                # write the sentence ID (text ID & number of sentence), text, author, year, genre, source into the corpus
+                with open(f"{dir_path}/Corpus.csv", "a", encoding="latin-1") as outfile:
+                    for i in range(sentences_nr):
+                        outfile.write(f"{text_nr}-{i};{sentences[i].strip()};{row[1]};{row[2]};{row[3]};{row[4]}\n")
+            row_nr += 1
+
+createCorpus(dir_path)
